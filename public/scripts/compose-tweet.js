a5ec9fd78b9599664e5b2be471b96dc1fe1cc7c6
Alina Chalanuchpong <alina.c@alumni.ubc.ca>
2020-11-07 22:35:08 -0800

    loadtweets

--- public/scripts/compose-tweet.js
+++ public/scripts/compose-tweet.js
@@ -56,48 +56,24 @@ $(document).ready(function () {
     form.submit(event => {
       event.preventDefault();
       const formData = form.serialize();
-      console.log(formData);
-      renderTweets(data);
-      // submitTweet(formData).then(res => {
-      //   console.log(res);
-      //   renderTweets(data);
-      // });
+      loadTweets().then(res => {
+        renderTweets(res);
+      });
     });
   });
 });
+
+const loadTweets = function () {
+  return $.get("/tweets", function (data) {
+    return data;
+  });
+};
 
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
 
 // function that create tweet element
 const createTweetElement = function (tweetData) {
@@ -108,18 +84,18 @@ const createTweetElement = function (tweetData) {
                 src=${tweetData.user.avatars}
               />
               <h1>${tweetData.user.name}</h1>
-            </div>
-            <h2>${tweetData.user.handle}</h2>
-          </header>
-          <div class="tweet-body">
-            <p>${tweetData.content.text}</p>
-          </div>
-          <footer>
+              </div>
+              <h2>${tweetData.user.handle}</h2>
+              </header>
+              <div class="tweet-body">
+              <p>${tweetData.content.text}</p>
+              </div>
+              <footer>
             <p>${getCurrentTime(tweetData.created_at)}</p>
             <span>
-              <i class="material-icons"> favorite </i>
+            <i class="material-icons"> favorite </i>
             </span>
-          </footer>`;
+            </footer>`;
   $tweet = $tweet.append(html);
   return $tweet;
 };
@@ -133,7 +109,7 @@ const renderTweets = function (tweets) {
     $html.prepend(createTweetElement(tweet));
   });
   // takes return value and appens it to the tweets container
-  $("#tweets-container").append($html);
+  $(".tweet_container").append($html);
 };
 
 const getCurrentTime = function (date) {
